11b0b640704073714d3249a84457e5e0feb05fe2
Zaixing Mao <zaixing.mao@cern.ch>
2013-12-08 21:40:56 +0100

    Clean code for simplicity

--- python/Tools/Draw_2DMass_Chain.py
+++ python/Tools/Draw_2DMass_Chain.py
@@ -4,16 +4,20 @@ import ROOT as r
 import tool
 from operator import itemgetter
 import os
+import enVars
 
-psfile="2DMass_signal"
+
+DrawWhichSample = "signal"
+DrawSVMass = False
 
 r.gStyle.SetOptStat(0)
 
 CSVCut1 = 0.679
 
-#title = "t#bar{t}"
-title = "H -> hh"
+title = "H -> hh" if DrawWhichSample == "signal" else "t#bar{t}"
 
+
+psfile= "2DMass_" + DrawWhichSample
 if CSVCut1 == 0.679:
     title = title + "(1 medium b tag)"
     psfile = psfile + "_1_b_tag.eps"
@@ -24,15 +28,22 @@ if CSVCut1 == 0.679:
 #     title = title + "(no b tags)"
 #     psfile = psfile + "_no_b_tag.eps"
 
+signalLocation = enVars.signalLocation
+ttLocation = enVars.ttLocation
+signalEntries = enVars.signalEntries
+ttEntries = enVars.ttEntries
 
 #*******Open input file and find associated tree*******
 Chain = r.TChain("ttTreeFinal/eventTree")
-tool.addFiles(ch=Chain, dirName="/hdfs/store/user/zmao/H2hh-SUB-TT")
-#tool.addFiles(ch=Chain, dirName="/hdfs/store/user/zmao/tt-SUB-TT")
-# tool.addFiles(ch=Chain, dirName="/hdfs/store/user/zmao/ZZ2-SUB-TT")
+if DrawWhichSample == "signal":
+    tool.addFiles(ch=Chain, dirName=signalLocation, knownEventNumber=signalEntries)
+    knownSampleEntries=signalEntries
+    Location=signalLocation
+else:
+    tool.addFiles(ch=Chain, dirName=ttLocation, knownEventNumber=ttEntries)
+    knownSampleEntries=ttEntries
+    Location=ttLocation
 
-mjj = r.TH1F("mjj"," ", 40, 0, 200)
-mTauTau = r.TH1F("mTauTau"," ", 40, 0, 200)
 Mass = r.TH2F("2DMass"," ", 70, 0, 350, 60, 0, 300)
 
 lvClass = r.Math.LorentzVector(r.Math.PtEtaPhiM4D('double'))
@@ -43,8 +54,9 @@ J2 = lvClass()
 J3 = lvClass()
 J4 = lvClass()
 
-print 'Getting nEntries in sample...'
-total= Chain.GetEntries()
+total = knownSampleEntries if knownSampleEntries else Chain.GetEntriesFast()
+key =  "found" if not knownSampleEntries else "has"
+print "[%s] %s %d events" %(Location, key, total)
 for i in range(0, total):
     Chain.GetEntry(i)
     tool.printProcessStatus(iCurrent=i, total=total, processName = 'Looping sample')
@@ -57,13 +69,14 @@ for i in range(0, total):
     jetsList = sorted(jetsList, key=itemgetter(0), reverse=True)
     if not (jetsList[0][0] < CSVCut1):
         continue
-
-    for i in range(Chain.pt1.size()):
-        tau1.SetCoordinates(Chain.pt1.at(i), Chain.eta1.at(i), Chain.phi1.at(i), Chain.m1.at(i))
-        tau2.SetCoordinates(Chain.pt2.at(i), Chain.eta2.at(i), Chain.phi2.at(i), Chain.m2.at(i))
-        mTauTau.Fill((tau1+tau2).mass())
-        Mass.Fill((jetsList[0][1]+jetsList[1][1]).mass(), (tau1+tau2).mass())
-
+    if DrawSVMass:
+        for iMass in range(Chain.svMass.size()):
+            Mass.Fill((jetsList[0][1]+jetsList[1][1]).mass(), Chain.svMass.at(iMass))
+    else:
+        for iTauPair in range(Chain.pt1.size()):
+            tau1.SetCoordinates(Chain.pt1.at(iTauPair), Chain.eta1.at(iTauPair), Chain.phi1.at(iTauPair), Chain.m1.at(iTauPair))
+            tau2.SetCoordinates(Chain.pt2.at(iTauPair), Chain.eta2.at(iTauPair), Chain.phi2.at(iTauPair), Chain.m2.at(iTauPair))
+            Mass.Fill((jetsList[0][1]+jetsList[1][1]).mass(), (tau1+tau2).mass())
 
 print ''
 
@@ -75,3 +88,5 @@ Mass.GetYaxis().SetTitleOffset(1.2)
 Mass.Draw("COLZ")
 
 ps.Close()
+
+print "Plot saved at %s" %(psfile)
