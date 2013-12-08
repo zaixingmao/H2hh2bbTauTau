1657094378ce00f0d3bd1775e8cafaf671f49eb6
Zaixing Mao <zaixing.mao@cern.ch>
2013-12-08 21:41:11 +0100

    test

--- python/Tools/test.py
+++ python/Tools/test.py
@@ -3,6 +3,11 @@
 import ROOT as r
 import os
 
-os.environ.keys()
-plotPath = os.environ['PYPATH']+'/Plots/'
-print plotPath
+ChainHistList = [('a', '1'),
+                 ('b', '2'),
+                 ('c', '3')]
+
+print "size: %d" %(len(ChainHistList))
+
+for i in range(3):
+    print ChainHistList[0][0]
