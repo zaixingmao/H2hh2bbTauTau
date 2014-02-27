2ba5f02d87e859dec39b44ce007b68eb3b18c59f
Zaixing <zaixing.mao@cern.ch>
2014-02-27 14:32:07 -0600

    add ability to same nEvents

--- python/Tools/cutSamples.py
+++ python/Tools/cutSamples.py
@@ -6,25 +6,37 @@ from operator import itemgetter
 import os
 import enVars
 from array import array
+import optparse
 
 lvClass = r.Math.LorentzVector(r.Math.PtEtaPhiM4D('double'))
 J1 = lvClass()
 J2 = lvClass()
 J3 = lvClass()
 J4 = lvClass()
+CSVJet1 = lvClass()
+CSVJet2 = lvClass()
 tau1 = lvClass()
 tau2 = lvClass()
 combinedJJ = lvClass()
 sv4Vec = lvClass()
+
+def opts():
+    parser = optparse.OptionParser()
+    parser.add_option("-n", dest="nevents", default="-1", help="amount of events to be saved")
+    options, args = parser.parse_args()
+
+    return options
+
+options = opts()
 
 
 def findFullMass(jetsList = [], sv4Vec = ''):
     jetsList = sorted(jetsList, key=itemgetter(0), reverse=True)
     combinedJJ = jetsList[0][1]+jetsList[1][1]
     if jetsList[1][0] > 0 and jetsList[0][1].pt() > 0 and jetsList[1][1].pt() > 0:
-        return combinedJJ, jetsList[0][0], jetsList[1][0], (combinedJJ+sv4Vec).mass(), r.Math.VectorUtil.DeltaR(jetsList[0][1], jetsList[1][1])
+        return combinedJJ, jetsList[0][0], jetsList[1][0], jetsList[0][1], jetsList[1][1], (combinedJJ+sv4Vec).mass(), r.Math.VectorUtil.DeltaR(jetsList[0][1], jetsList[1][1])
     else:
-        return -1, -1, -1, -1, -1
+        return -1, -1, -1, -1, -1, -1, -1
 
 r.gStyle.SetOptStat(0)
 
@@ -35,12 +47,14 @@ ZZEntries = enVars.ZZEntries
 #*******Get Sample Name and Locations******
 sampleLocations = enVars.sampleLocations
 
-varList = ['EVENT', 'HMass', 'svMass', 'svPt', 'svEta', 'svPhi', 'J1Pt', 'J1Eta','J1Phi', 'J1Mass', 'NBTags', 'iso1', 'iso2', 'mJJ', 'J2Pt', 'J2Eta','J2Phi', 'J2Mass','pZeta', 'pZ',
-           'pZV', 'J3Pt', 'J3Eta','J3Phi', 'J3Mass', 'J4Pt', 'J4Eta','J4Phi', 'J4Mass', 'J1CSVbtag', 'J2CSVbtag', 'J3CSVbtag', 'J4CSVbtag', 'pt1', 'eta1', 'phi1', 'pt2', 'eta2', 'phi2']
+varList = ['EVENT', 'HMass', 'svMass', 'svPt', 'svEta', 'svPhi', 'J1Pt', 'J1Eta','J1Phi', 'J1Mass', 'NBTags', 'iso1', 'iso2', 'mJJ', 'J2Pt', 'J2Eta','J2Phi', 'J2Mass','pZeta', 'pZ', 'm1', 'm2',
+           'pZV', 'J3Pt', 'J3Eta','J3Phi', 'J3Mass', 'J4Pt', 'J4Eta','J4Phi', 'J4Mass', 'J1CSVbtag', 'J2CSVbtag', 'J3CSVbtag', 'J4CSVbtag', 'pt1', 'eta1', 'phi1', 'pt2', 'eta2', 'phi2', 'met']
+
+blackList = enVars.corruptedROOTfiles
 
 for iSample, iLocation in sampleLocations:
-    iChain = r.TChain("ttTreeIni/eventTree")
-    nEntries = tool.addFiles(ch=iChain, dirName=iLocation, knownEventNumber=signalEntries, printTotalEvents=True)
+    iChain = r.TChain("ttTreeFinal/eventTree")
+    nEntries = tool.addFiles(ch=iChain, dirName=iLocation, knownEventNumber=signalEntries, printTotalEvents=True, blackList=blackList)
     iChain.SetBranchStatus("*",0)
     for iVar in range(len(varList)):
         iChain.SetBranchStatus(varList[iVar],1)
@@ -54,9 +68,16 @@ for iSample, iLocation in sampleLocations:
     dRTauTau = array('f', [0.])
     dRJJ = array('f', [0.])
     dRhh = array('f', [0.])
+    mTop1 = array('f', [0.])
+    mTop2 = array('f', [0.])
+    pZ_new = array('f', [0.])
+    pZV_new = array('f', [0.])
+    pZ_new2 = array('f', [0.])
+    pZV_new2 = array('f', [0.])
     iChain.LoadTree(0)
     iTree = iChain.GetTree().CloneTree(0)
-    iFile = r.TFile("%s.root" %(iSample),"recreate")
+    iSample = iSample + '_%s' %('all' if options.nevents == -1 else options.nevents)
+    iFile = r.TFile("%s.root" %(iSample, ),"recreate")
     iTree.Branch("fMass", fullMass, "fMass/F")
     iTree.Branch("mJJ", mJJ, "mJJ/F")
     iTree.Branch("etaJJ", etaJJ, "etaJJ/F")
@@ -67,12 +88,19 @@ for iSample, iLocation in sampleLocations:
     iTree.Branch("dRTauTau", dRTauTau, "dRTauTau/F")
     iTree.Branch("dRJJ", dRJJ, "dRJJ/F")
     iTree.Branch("dRhh", dRhh, "dRhh/F")
-
+    iTree.Branch("mTop1", mTop1, "mTop1/F")
+    iTree.Branch("mTop2", mTop2, "mTop2/F")
+    iTree.Branch("pZ_new", pZ_new, "pZ_new/F")
+    iTree.Branch("pZV_new", pZV_new, "pZV_new/F")
+    iTree.Branch("pZ_new2", pZ_new2, "pZ_new2/F")
+    iTree.Branch("pZV_new2", pZV_new2, "pZV_new2/F")
     counter = 0
 
     for iEntry in range(nEntries):
         iChain.LoadTree(iEntry)
         iChain.GetEntry(iEntry)
+        if counter == int(options.nevents):
+            break
         if iChain.svMass.size() == 0:
             continue
         jetsList = [(iChain.J1CSVbtag, J1.SetCoordinates(iChain.J1Pt, iChain.J1Eta, iChain.J1Phi, iChain.J1Mass)),
@@ -81,15 +109,23 @@ for iSample, iLocation in sampleLocations:
                     (iChain.J4CSVbtag, J4.SetCoordinates(iChain.J4Pt, iChain.J4Eta, iChain.J4Phi, iChain.J4Mass))]
         sv4Vec.SetCoordinates(iChain.svPt.at(0), iChain.svEta.at(0), iChain.svPhi.at(0), iChain.svMass.at(0))
         bb = lvClass()
-        bb, CSVJ1[0], CSVJ2[0], fullMass[0], dRJJ[0] = findFullMass(jetsList=jetsList, sv4Vec=sv4Vec) 
+        bb, CSVJ1[0], CSVJ2[0], CSVJet1, CSVJet2, fullMass[0], dRJJ[0] = findFullMass(jetsList=jetsList, sv4Vec=sv4Vec) 
         if bb == -1:
             continue
         ptJJ[0] = bb.pt()
         etaJJ[0] = bb.eta()
         phiJJ[0] = bb.phi()
         mJJ[0] = bb.mass()
-        tau1.SetCoordinates(iChain.pt1.at(0), iChain.eta1.at(0), iChain.phi1.at(0), 0)
-        tau2.SetCoordinates(iChain.pt2.at(0), iChain.eta2.at(0), iChain.phi2.at(0), 0)
+        tau1.SetCoordinates(iChain.pt1.at(0), iChain.eta1.at(0), iChain.phi1.at(0), iChain.m1.at(0))
+        tau2.SetCoordinates(iChain.pt2.at(0), iChain.eta2.at(0), iChain.phi2.at(0), iChain.m2.at(0))
+        mTop1[0] = (CSVJet1 + tau1).mass()
+        mTop2[0] = (CSVJet2 + tau2).mass()
+
+        pZ_new[0] = iChain.pZ/iChain.svPt.at(0)
+        pZV_new[0] = iChain.pZV/iChain.svPt.at(0)
+        pZ_new2[0] = iChain.pZ/fullMass[0]
+        pZV_new2[0] = iChain.pZV/fullMass[0]
+
         dRTauTau[0] = r.Math.VectorUtil.DeltaR(tau1, tau2)
         dRhh[0] = r.Math.VectorUtil.DeltaR(bb, sv4Vec)
         iTree.Fill()
